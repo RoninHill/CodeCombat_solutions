cb53d1659379d2916900a095f7f7f4a2b2862d31
Nico Poblete <nlypoblet@gmail.com>
2017-05-18 23:18:21 -0700

    Added second_kithmaze

---
+++ src/dungeon/second_kithmaze.lua
@@ -0,0 +1,9 @@
+-- Use a while-true loop to navigate the maze!
+
+while true do
+    -- Move right, up, right, down
+    hero:moveRight()
+    hero:moveUp()
+    hero:moveRight()
+    hero:moveDown()
+end
