53ca98a3379202e8b5e6536d649ca06330aafcde
Nico Poblete <nlypoblet@gmail.com>
2017-05-18 23:17:43 -0700

    Added second_kithmaze

---
+++ src/dungeon/second_kithmaze.py
@@ -0,0 +1,9 @@
+# Use a while-true loop to navigate the maze!
+
+while(True):
+    # Move right, up, right, down
+    hero.moveRight()
+    hero.moveUp()
+    hero.moveRight()
+    hero.moveDown()
+
